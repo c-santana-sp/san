6fd1f9930039f95bca4d0aa1c4caa74daa416a07
c-santana-sp <eduardomarquesdesantana@gmail.com>
2022-05-03 01:19:29 -0300

    feat: creating Lexer base class

---
+++ lib/san/lexer.rb
@@ -0,0 +1,27 @@
+module San
+  class Lexer
+
+    attr_reader :source, :tokens
+
+    def initialize(source)
+      @source = source
+      @tokens = []
+      @line = 0
+      @next_p = 0
+      @lexeme_start_p = 0
+    end
+
+    def start_tokenization
+      tokenize()
+
+      tokens
+    end
+
+    private
+      attr_accessor :line, :next_p, :lexeme_start_p
+
+    def tokenize
+    end
+
+  end
+end
